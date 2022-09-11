0b2015cca3cb2d0b630f2d06edea7115ebc033d4
gaboliveiradev <g38789138@gmail.com>
2022-09-11 20:31:16 -0300

    BUILDANDO A CLASSE HOSPEDAGEM E TROCANDO TIPO DA CLASSE QUARTO

--- AppHotelV2/Model/Hospedagem.cs
+++ AppHotelV2/Model/Hospedagem.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace AppHotelV2.Model
 {
-    internal class Hospedagem
+    public class Hospedagem
     {
+        Quarto suite;
+        public int QntAdultos { get; set; }
+        public int QntCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public Quarto Suite
+        {
+            get => suite;
+
+            set
+            {
+                if (value == null) throw new Exception("Por favor, selecione uma suíte");
+                suite = value;
+            }
+        }
+
+        public int Estadia
+        {
+            get
+            {
+                return DataCheckOut.Subtract(DataCheckIn).Days;
+            }
+        }
+
+        public double ValorTotal
+        {
+            get
+            {
+                return ((QntAdultos * Suite.ValorDiariaAdulto) + (QntCriancas * Suite.ValorDiariaCrianca)) * Estadia;
+            }
+        }
+    }
+}
+
+            }
+        }
+
     }
 }
--- AppHotelV2/Model/Quarto.cs
+++ AppHotelV2/Model/Quarto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AppHotelV2.Model
 {
-    internal class Quarto
+    public class Quarto
     {
     }
 }
