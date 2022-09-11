00772e9192aa53b53dfbdb2b1ea435c40aa43acd
gaboliveiradev <g38789138@gmail.com>
2022-09-11 20:53:00 -0300

    TROCANDO A PÁGINA DE INICIALIZAÇÃO + CRIANDO UM LIST DE QUARTOS

--- AppHotelV2/App.xaml.cs
+++ AppHotelV2/App.xaml.cs
@@ -2,15 +2,45 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+using AppHotelV2.Model;
+using AppHotelV2.View;
+
 namespace AppHotelV2
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Premium",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Confort",
+                ValorDiariaAdulto = 240.0,
+                ValorDiariaCrianca = 120.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Deluxe",
+                ValorDiariaAdulto = 380.0,
+                ValorDiariaCrianca = 160.0
+            }
+        };
+
         public App()
         {
             InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new OrcamentoHospedagem());
         }
 
         protected override void OnStart()
