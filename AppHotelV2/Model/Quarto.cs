87acff8270fd935eaa3feb8670bda0f45a5b76d2
gaboliveiradev <g38789138@gmail.com>
2022-09-11 20:33:08 -0300

    BUILDANDO A CLASSE QUARTO E DEFININDO SUAS PROPRIEDADES NECESSÁRIAS

--- AppHotelV2/Model/Quarto.cs
+++ AppHotelV2/Model/Quarto.cs
@@ -6,5 +6,8 @@ namespace AppHotelV2.Model
 {
     public class Quarto
     {
+        public string Descricao { get; set; }
+        public double ValorDiariaAdulto { get; set; }
+        public double ValorDiariaCrianca { get; set; }
     }
 }
