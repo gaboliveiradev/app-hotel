b5ee266c5d4a1aaab6eabe0a0acbb3e9584f2080
gaboliveiradev <g38789138@gmail.com>
2022-09-11 21:58:38 -0300

    TROCANDO A COR DA BARRA DE STATUS

--- AppHotelV2.Android/MainActivity.cs
+++ AppHotelV2.Android/MainActivity.cs
@@ -17,6 +17,10 @@ namespace AppHotelV2.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+            Window.AddFlags(Android.Views.WindowManagerFlags.DrawsSystemBarBackgrounds);
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#232323"));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
