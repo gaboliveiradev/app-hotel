b824945fcd8a9150e4cd3829f563d6c672c477e4
gaboliveiradev <g38789138@gmail.com>
2022-09-11 23:12:06 -0300

    login finalizado

--- AppHotelV2/View/Login.xaml.cs
+++ AppHotelV2/View/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppHotelV2.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,40 @@ namespace AppHotelV2.View
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                List<DadosUsuario> list_usuarios = new List<DadosUsuario>
+                {
+                    new DadosUsuario
+                    {
+                        Usuario = "Gabriel Oliveira",
+                        Senha = "dev@2022"
+                    },
+                    new DadosUsuario
+                    {
+                        Usuario = "Aluno",
+                        Senha = "etecjau"
+                    }
+                };
 
+                DadosUsuario dados_digitados = new DadosUsuario()
+                {
+                    Usuario = txt_usuario.Text,
+                    Senha = txt_senha.Text
+                };
+
+                if(list_usuarios.Any(i => (i.Usuario == dados_digitados.Usuario && i.Senha == dados_digitados.Senha)))
+                {
+                    App.Current.Properties.Add("usuario_logado", txt_usuario.Text);
+                    App.Current.MainPage = new OrcamentoHospedagem();
+                } else
+                {
+                    throw new Exception("Dados Incorretos, tente novamente.");
+                }
+            } catch (Exception err)
+            {
+                DisplayAlert("Ooops", err.Message, "OK");
+            }
         }
     }
 }
