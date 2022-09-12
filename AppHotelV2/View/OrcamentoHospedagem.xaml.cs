151caa9ba13be614100a05460b3021110d1e4e29
gaboliveiradev <g38789138@gmail.com>
2022-09-11 21:43:00 -0300

    METODOS DATE_SELECTED + CALCULO DE HOSPEDAGEM E TROCANDO DE PÁGINA + CONFIGURANDO O PICKER E DATE PICKER

--- AppHotelV2/View/OrcamentoHospedagem.xaml.cs
+++ AppHotelV2/View/OrcamentoHospedagem.xaml.cs
@@ -6,25 +6,62 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AppHotelV2.Model;
+using AppHotelV2.View;
+
 
 namespace AppHotelV2.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OrcamentoHospedagem : ContentPage
     {
+
+        App PropriedadesApp;
+
         public OrcamentoHospedagem()
         {
             InitializeComponent();
+            PropriedadesApp = (App)Application.Current;
+
+            pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+            dtpck_checkin.MinimumDate = DateTime.Now;
+            dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+            dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+            dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
         }
 
         private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
         {
+            DatePicker elemento = (DatePicker)sender;
+            DateTime data_selecionada_checkin = elemento.Date;
 
+            dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+            dtpck_checkout.MaximumDate = data_selecionada_checkin.Date.AddMonths(6);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                Hospedagem h = new Hospedagem()
+                {
+                    QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                    QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                    Suite = (Quarto)pck_quarto.SelectedItem,
+                    DataCheckIn = dtpck_checkin.Date,
+                    DataCheckOut = dtpck_checkout.Date
+                };
 
+                Navigation.PushAsync(new ExtratoHospedagem()
+                {
+                    BindingContext = h
+                });
+            } catch (Exception err)
+            {
+                DisplayAlert("Ops", err.Message, "OK");
+            }
         }
     }
 }
