a03db246ffa4621636875af3714ed47002e7e825
gaboliveiradev <g38789138@gmail.com>
2022-09-11 22:43:59 -0300

    buildando a classe DadosUsuario.cs

---
+++ AppHotelV2/Model/DadosUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHotelV2.Model
+{
+    public class DadosUsuario
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
